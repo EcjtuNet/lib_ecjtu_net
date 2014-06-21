fd734202dc725c9177af7d0e96e02a8aa990324f
wtbhk <wtbhk@Live.cn>
2014-06-21 21:37:13 +0800

    移动位置

--- core/Page/Parser/Parser.py
+++ core/Page/Parser/Parser.py
@@ -1,2 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import re
+
+class Parser():
+	def parse(self):
+		pass
--- core/Parser.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import re
-
-class Parser():
-	def parse(self):
-		pass
