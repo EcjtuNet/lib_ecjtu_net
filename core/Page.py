31079fc31ec64888e631ef4eb899e3ec76bd1650
wtbhk <wtbhk@Live.cn>
2014-06-21 21:20:55 +0800

    修改位置

--- core/Page.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import sys
-sys.path.append("Parser/")
-from Request import Request
-
-class Page:
-	def __init__(self):
-		pass
--- core/Page/Page.py
+++ core/Page/Page.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import sys
+sys.path.append("Parser/")
+from Request import Request
+
+class Page:
+	def __init__(self):
+		pass
